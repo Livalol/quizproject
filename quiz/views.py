97c7425a19ddf67bf1a6a72f121625e1177f76a1
Livalol <liva.lager@doberman.se>
2017-09-27 11:03:36 +0200

    #9 Created 4 new views in views

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def start(request):
+	return render(request, "start.html")
+
+def quize(request, quiz_number):
+	return render(request, "quize.html")
+
+def questions(request, quiz_number, question_number):
+	return render(request, "questions.html")
+
+def results(request, quiz_number):
+	return render(request, "results.html")
